3397c136272190e5ec68d5878d6395cc5cf43dd7
user01377 <144172972+user01377@users.noreply.github.com>
2023-10-28 18:43:49 -0500

    update to code

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from ppadb.client import Client as AdbClient
 from PIL import Image
 from PIL import ImageStat
 import pyautogui
 import time
+
+client = AdbClient(host="127.0.0.1", port=5037) # Default is "127.0.0.1" and 5037
+devices = client.devices()
+#checks if device is connected to PC properly
+if len(devices) == 0:
+    print('No devices')
+    quit()
+
+device = devices[0]
+print(f'Connected to {device}')
 
 #returns avg value of pixels in a png
 def brightness( im_file ):
@@ -11,11 +22,15 @@ def brightness( im_file ):
    #255 brighest, 0 darkest
 
 while True:
-   pyautogui.screenshot("downloads/test.png")
-   im_file = "downloads/test.png"
+   pyautogui.screenshot("downloads/currentState.png")
+   im_file = "downloads/currentState.png"
    time.sleep(0.25)
    if brightness(im_file) >= 130:
-      #turn on flash
-   elif brightness(im_file) in range(45,71):
-      #turn on flash
+      device.shell("input touchscreen tap 667 1502")
+   elif brightness(im_file) not range(45,71):
+      device.shell("input touchscreen tap 667 1502")
+   elif brightness(im_file) #notin range(45,71)
+      device.shell("input touchscreen tap 667 1502")
+   elif brightness(im_file) < 130:
+      brightness(im_file) >= 130:
    print("program has ran")
