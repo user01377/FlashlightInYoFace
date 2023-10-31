e6f27a45fddc7173c1768c856e61b76a06ccb29d
user01377 <144172972+user01377@users.noreply.github.com>
2023-10-30 23:30:38 -0500

    REALLY fixed new code to check if flashlight on/off

--- main.py
+++ main.py
@@ -22,23 +22,23 @@ def brightness( im_file ):
    #255 brighest, 0 darkest
 
 flashlightIsOn = False
+doubleBrightCheck = False
 
 #constanly runs code, probably could ask if user wanted to stop code
 while True:
    #screenshots user's screen, saves that file to a variable
-   pyautogui.screenshot("downloads/currentState.png")
-   im_file = "downloads/currentState.png"
-   
-   if brightness(im_file) >= 130 or brightness(im_file) in range(45,71) and flashlightIsOn == False:
+   pyautogui.screenshot("./currentState.png")
+   im_file = "./currentState.png"
+
+   if brightness(im_file) >= 130 or brightness(im_file) in range(45,71):
+      doubleBrightCheck = True
+
+   if doubleBrightCheck == True and flashlightIsOn == False:
       device.shell("input touchscreen tap 667 1502")
       flashlightIsOn = True
+      doubleBrightCheck = False
+
    elif flashlightIsOn == True and brightness(im_file) <= 129 and brightness(im_file) not in range(45,71):
       device.shell("input touchscreen tap 667 1502")
       flashlightIsOn = False
    
-
-#while brightness >= 130, keep light on
-#if it dips below 130, turn off light
-
-#while brightness in range(45,71), turn on flashlight
-#if it is NOT in this range, turn off flashlight
